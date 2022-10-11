a90dfcb80d16ac2b61d655073d87eb3af442e3fe
opee2012 <jborth2010@gmail.com>
2022-10-11 14:43:06 -0400

    added triple calculations

--- index2.php
+++ index2.php
@@ -20,32 +20,36 @@
         $dice3 = rand(1,6);
         echo "<img src='images/Alea_$dice3.png'><br>";
 
-        $diceall = $dice1 + $dice2 + $dice3;
-
+        $pot = array(0, 0, 0, 0);
         $big = (int) $_POST["bigbet"];
         $small = (int) $_POST["smallbet"];
         $even = (int) $_POST["evenbet"];
         $odd = (int) $_POST["oddbet"];
 
-        $pot = array(0, 0, 0, 0);
-            
-        if ($diceall >= 11 && $diceall <= 17 && $big >= 0) {
-            $pot[0] = $big;
-        } else $pot[0] = 0 - $big;
+        $diceall = $dice1 + $dice2 + $dice3;
 
-        if ($diceall >= 4 && $diceall <= 10 && $small >= 0) {
-            $pot[1] = $small;
-        } else $pot[1] = 0 - $small;
-
-        if ($diceall % 2 == 0 && $even >= 0) {
-            $pot[2] = $even;
-        } else $pot[2] = 0 - $even;
-
-        if ($diceall % 2 == 1 & $odd >= 0) {
-            $pot[3] = $odd;
-        } else $pot[3] = 0 - $odd;
-
-        echo "$pot[0]\n$pot[1]\n$pot[2]\n$pot[3]<br>";
+        if ($dice1 == $dice2 && $dice2 = $dice3) {
+            $pot[0] = 0 - $big;
+            $pot[1] = 0 - $small;
+            $pot[2] = 0 - $even;
+            $pot[3] = 0 - $odd;
+        } else {
+            if ($diceall >= 11 && $diceall <= 17 && $big >= 0) {
+                $pot[0] = $big;
+            } else $pot[0] = 0 - $big;
+    
+            if ($diceall >= 4 && $diceall <= 10 && $small >= 0) {
+                $pot[1] = $small;
+            } else $pot[1] = 0 - $small;
+    
+            if ($diceall % 2 == 0 && $even >= 0) {
+                $pot[2] = $even;
+            } else $pot[2] = 0 - $even;
+    
+            if ($diceall % 2 == 1 && $odd >= 0) {
+                $pot[3] = $odd;
+            } else $pot[3] = 0 - $odd;
+        }
 
         $betTotal = $pot[0] + $pot[1] + $pot[2] + $pot[3];
         $balance = (int) $_POST["balance"];
